0950ec9b39801ec51a9e7c2c4494682884bb31e1
EfiokobongN <mailpreciousnelson@gmail.com>
2024-08-19 16:53:31 +0100

    migration for product and review made

--- database/migrations/2024_08_19_140550_create_products_table.php
+++ database/migrations/2024_08_19_140550_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('price');
+            $table->text('detail');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
