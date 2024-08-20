90e7cf1a4344856b57c1a4b88171b7ca441c7784
EfiokobongN <mailpreciousnelson@gmail.com>
2024-08-20 12:11:20 +0100

    modified product details and show single product

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out Of Stock' : $this->stock,
             'discount' => $this->discount,
+            'TotalPrice' => round((1 - ($this->discount/100))*$this->price,2), // substracting discount price from Amount 17/100 = .17, 1 - .17 = .83, .83*price
+            'rating' => $this->reviews->count() > 0 ?round($this->reviews->sum('star')/$this->reviews->count(),1) : 'No Rating', // relationship name in the product model
+            'href' => [
+                'review' => route('review.index', $this->id),
+            ]
         ];
     }
 }
