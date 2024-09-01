3fea8d17860b1aa8a2de1dab967a894ed0ddf074
EfiokobongN <mailpreciousnelson@gmail.com>
2024-09-01 22:27:58 +0000

    review update

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -70,9 +70,14 @@ class ReviewController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateReviewRequest $request, Review $review)
+    public function update(UpdateReviewRequest $request, Product $product, Review $review)
     {
         //
+        $review->update($request->all());
+
+        return response([
+            'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
     }
 
     /**
--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'customer' => $this->customer,
             'Message' => $this->review,
             'Rate' => $this->star,
